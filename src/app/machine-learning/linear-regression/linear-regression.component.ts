9651440bbe7f58b1cb52950cff9e6e51afd59531
swarup4 <swarup.saha004@hotmail.com>
2022-05-27 03:24:30 +0530

    Edit Project Functionality

--- src/app/machine-learning/linear-regression/linear-regression.component.ts
+++ src/app/machine-learning/linear-regression/linear-regression.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { AddProjectComponent } from 'src/app/add-project/add-project.component';
 import { ProjectService } from 'src/app/add-project/project.service';
 import { SeeMoreComponent } from 'src/app/common/see-more/see-more.component';
 
@@ -19,7 +20,7 @@ export class LinearRegressionComponent implements OnInit {
 
   constructor(private projectService: ProjectService, private dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  getProject(){
     this.projectService.getProject('linear').subscribe((res: any) => {
       let data = res.map((element: any) => ({ ...element, theme: this.getTheme() }));
       this.project = data;
@@ -27,24 +28,42 @@ export class LinearRegressionComponent implements OnInit {
       console.log(err);
     });
   }
+  ngOnInit(): void {
+    this.getProject();
+  }
 
   getTheme() {
     const theme = this.themeList[Math.floor(Math.random() * this.themeList.length)];
     return theme;
   }
 
-  seeMore(heading: string, details: string){
+  seeMore(heading: string, details: string) {
     const dialogRef = this.dialog.open(SeeMoreComponent, {
-      data: { 
+      data: {
         heading: heading,
         details: details
       }
     });
-    
+
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
   }
 
+  edit(id: string, name: string, description: string) {
+    const dialogRef = this.dialog.open(AddProjectComponent, {
+      data: {
+        id: id,
+        name: name,
+        description: description
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result.data != ""){
+        this.getProject();
+      }
+    });
+  }
 
 }
